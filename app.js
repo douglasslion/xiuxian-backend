0b9bffacdbdc9b8c7ea5af35d43fcf33ee302a95
douglasslion <119070663@qq.com>
2026-03-24 20:52:00 +0800

    更新app.js文件，添加GitHub提示信息

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ status: 'error', message: '服务器内部错误' });
 });
 
-// 启动服务
+// 启动服务为了提示GitHub添加的信息
 const PORT = process.env.PORT || 8002;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`服务运行在 0.0.0.0:${PORT}`);
